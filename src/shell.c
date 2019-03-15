2a4903eafce2c04dbcb61f8dd4f4bac671ca2755
Michael McDonagh VM <mjmcdonagh@cs.stonybrook.edu>
2019-03-15 15:30:04 +0000

    Added file to contain code for operating the shell, and added a function for splitting a command line string into its component arguments

---
+++ src/shell.c
@@ -0,0 +1,55 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <ctype.h>
+#include <getopt.h>
+#define BOOLEAN char
+#define FALSE 0
+#define TRUE 1
+
+
+#define MAXARGS 5
+char** commandSplitter(char* commandLine){
+	while (isspace(*commandLine)) commandLine++;
+	if (*commandLine == '\0'){
+		printf("ERROR: failed to parse command line\n");
+		return NULL;
+	}
+	//commandLine = malloc(strlen(commandLine)*sizeof(char)+1);
+	char** output = malloc(MAXARGS*sizeof(char*));
+	int inputCheck = 0;
+	int index = 0;
+	output[index] = commandLine;
+	
+	while (*++commandLine != '\0' && index < MAXARGS){
+		if (*commandLine == ' '){
+			*commandLine = '\0';
+			while (isspace(*++commandLine));
+			if (*commandLine == '\"'){
+				output[++index] = ++commandLine;
+				while (*commandLine != '\"'){
+					if (*commandLine == '\0'){
+						printf("ERROR: failed to parse command line\n");
+						return NULL;
+					}
+					commandLine++;
+				}
+				*commandLine = '\0';
+				while(isspace(*++commandLine));
+			}
+			else{
+				output[++index] = commandLine;
+			}
+		}
+	}
+	return output;
+}
+
+void shell_loop(){
+	char* commandLine;
+	size_t bufsize = 256;
+	
+	do{
+		printf("> ");
+		getline(&commandLine, &bufsize, stdin);
+	} while (1);
+}
